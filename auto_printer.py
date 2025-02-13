2cca8917e25b7a5e8ad36b6615f02030475f1401
DanhAllen <danhpc1992@gmail.com>
2025-02-13 14:07:09 +0700

    upgrade note of code

--- auto_printer.py
+++ auto_printer.py
@@ -2,11 +2,10 @@ import os
 import requests # module get url 
 import subprocess #module auto install file
 import zipfile #extract File 
-import shutil
 
     
 def download_file(url, save_folder, printer_driver):
-    # Create the full path for saving the file
+    # Sau khi download file từ url thì sẽ kết hợp file về folder và tên file
     save_path = os.path.join(save_folder, printer_driver)
 
     # Send a GET request to the URL
@@ -23,8 +22,9 @@ def download_file(url, save_folder, printer_driver):
         print(f"File downloaded successfully and saved to {save_path}")
     else:
         print(f"Failed to download file. Status code: {response.status_code}")
-    return save_path
+    return save_path # cần sử dụng return để cho phép sau khi check thì trả lại hàm 
 
+# Hàm giải nén file 
 def extract_zip(zip_path, extract_to):
     try:
         """Extract a ZIP file to a specified directory."""
@@ -35,7 +35,7 @@ def extract_zip(zip_path, extract_to):
             print(f"Error: The file {zip_path} is not a valid ZIP file.")
     except Exception as e:
             print(f"An error occurred while extracting the ZIP file: {e}")
-
+# Hàm auto run file.exe
 def install_driver(installer_path):
     """Run the installer executable."""
     if os.path.exists(installer_path):
@@ -50,14 +50,14 @@ def install_driver(installer_path):
         print(f"Installer not found: {installer_path}")
 
 
-# Định nghĩa các tên chính sẽ được sử dụng trong function 
+# Câu lệnh bên dưới cho pháp chạy các tính năng chính có trong hàm phía trên 
 if __name__ == "__main__":
     # URL of the printer driver ZIP file
     url = "https://business.toshiba.com/downloads/KB/f1Ulds/21280/ebn-Uni-7.222.5412.231.zip"
     
     # Folder to save the downloaded file
     save_folder = r"C:\Users\Public\Downloads"  # Change to your desired path
-    zip_filename = "printer_driver.zip"
+    zip_filename = "printer_driver.zip" # chaneg name file when was download 
     
     # Step 1: Download the ZIP file
     zip_path = download_file(url, save_folder, zip_filename)
@@ -69,7 +69,8 @@ if __name__ == "__main__":
 
         # Step 3: Find the installer executable (adjust the name as necessary)
         installer_name = "essetup.exe"  # Change this to the actual installer name if different
-        installer_path = os.path.join(r"C:\Users\Public\Downloads\UNI", installer_name)
+        installer_path = os.path.join(r"C:\Users\Public\Downloads\UNI", installer_name) 
+        #Nếu file giải nén có thêm 1 folder khác cần thay đổi đường dẫn nơi chưa file cài đặt
 
         # Step 4: Install the driver
         install_driver(installer_path)
